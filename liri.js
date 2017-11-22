af56afdd286815cd06cf4d89139aa0d7a96e5636
jdgrillone <jdgrillone@gmail.com>
2017-11-22 14:03:24 -0700

    spotify-this-song functionality working

--- liri.js
+++ liri.js
@@ -1,6 +1,14 @@
 //Include Twitter NPM package
 var Twitter = require("twitter");
 
+//Include Spotify NPM package
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+	id: 'f8dca6b84e7c4de1888cef30cd5ef9be',
+	secret: 'c351e67dcf694da0b369f7eaf8dac7e7'
+});
+
 //Variable containing Twitter keys
 var keys = require("./keys.js");
 
@@ -10,6 +18,7 @@ var client = new Twitter({
 	access_token_key: keys.access_token_key,
 	access_token_secret: keys.access_token_secret
 });
+
 
 //Variables to contain commands
 var operation = process.argv[2];
@@ -22,7 +31,7 @@ if (operation === "my-tweets") {
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if (!error) {
 			console.log("A");
-			for (i = 0; i < 20; i++){
+			for (i = 0; i < 3; i++){
 				console.log(tweets[i].created_at);
 				console.log(tweets[i].text);
 			}
@@ -32,6 +41,23 @@ if (operation === "my-tweets") {
 //Spotify call
 else if (operation === "spotify-this-song") {
 	console.log("Here is the song information.");
+	spotify.search({ type: 'track', query: value, limit: 1 }, function(err, data) {
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+		//Logs the artist name
+		console.log("===Artist===\n", data.tracks.items[0].album.artists[0].name);
+		//Logs the song name
+		console.log("===Song===\n", data.tracks.items[0].name);
+		//Logs the song URL
+		if (data.tracks.items[0].preview_url === null) {
+			console.log("===Preview===\nSorry, there is not a preview available for that track.")
+		}else {
+			console.log("===Preview===\n", data.tracks.items[0].preview_url);
+		}
+		//Logs the album
+		console.log("===Album===\n", data.tracks.items[0].album.name);
+	});
 }
 //OMDB call
 else if (operation === "movie-this") {
