0f8d96373e2c1349aacf0fc5f4c94d7dba236d3e
jdgrillone <jdgrillone@gmail.com>
2017-11-22 11:11:15 -0700

    initialized npm package, twitter keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'Ng8CFJxImBDve8jYpABJkkb8p',
+  consumer_secret: 'dkn1PpEtg8J576XWd8QxW1gz44lxKnuNlDAKlIn4rs7kStPY20',
+  access_token_key: '933394425538940928-9u3y8zktPLxAWaa4tAXpWdwOdFPAmPf',
+  access_token_secret: '7hWjvnWG2GaDAzGneNkeryqYGUOfF5XQqIqnASjGVdJ68',
+}
+
+module.exports = twitterKeys;
